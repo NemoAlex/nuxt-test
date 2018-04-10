3659404a9feea930ad3aa6611b64d2951351456a
Nemo <zhhjchina@gmail.com>
2018-04-10 16:32:16 +0800

    init

---
+++ pages/_name.vue
@@ -0,0 +1,21 @@
+<style>
+.nuxt-link-active {
+  color: white;
+  background: green;
+}
+</style>
+
+<template>
+<div>
+  <nuxt-link :to="'/' + name">{{ name }}</nuxt-link>
+</div>
+</template>
+
+<script>
+export default {
+  asyncData ({ params }) {
+    return { name: params.name }
+  }
+}
+</script>
+
